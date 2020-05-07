b2e27f8f152652eb719357aac9ea9046e7bf6aea
NicolasSwenson <ns578>
2020-05-06 22:16:41 -0700

    download shell

---
+++ data/download.sh
@@ -0,0 +1,6 @@
+# using Yu group Covid-19 dataset from UC Berkeley
+
+echo 'downloading county_data_abridged.csv'
+wget https://github.com/Yu-Group/covid19-severity-prediction/blob/master/data/county_data_abridged.csv
+
+echo 'find column descriptions at : https://github.com/Yu-Group/covid19-severity-prediction/blob/master/data/list_of_columns.md'
