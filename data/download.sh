4333367f6ed88325c29ae30c37c57eef521aa72b
NicolasSwenson <ns578>
2020-05-07 12:03:33 -0700

    add infection rates

--- data/download.sh
+++ data/download.sh
@@ -4,3 +4,6 @@ echo 'downloading county_data_abridged.csv'
 wget https://github.com/Yu-Group/covid19-severity-prediction/raw/master/data/county_data_abridged.csv
 
 echo 'find column descriptions at : https://github.com/Yu-Group/covid19-severity-prediction/blob/master/data/list_of_columns.md'
+
+echo 'downloading csse_covid_19_daily_reports/05-06-2020.csv'
+wget https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_daily_reports/05-06-2020.csv
